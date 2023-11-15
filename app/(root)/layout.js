bc2bab0dd1b3a4cf87bda4a7e3b8386de53b3e44
M4ks1m123 <maximalian67@gmail.com>
2023-11-15 09:14:35 +0300

    fixing docker

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,13 +1,8 @@
 import '../globals.css'
-import { Inter } from 'next/font/google'
 
 import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSideBar'
-import RightSidebar from '@/components/shared/RightSideBar'
-import Bottombar from '@/components/shared/BottomBar'
 import AuthProvider from './context/AuthProvider'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -17,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthProvider>
           <Topbar />
           <main className='bg-zinc-100'>
