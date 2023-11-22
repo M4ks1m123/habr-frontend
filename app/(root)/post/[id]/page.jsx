92bae3516da512b1465a5de4a27aad5a5a7453f6
M4ks1m123 <maximalian67@gmail.com>
2023-11-22 09:06:07 +0300

    added data fetching

--- app/(root)/post/[id]/page.jsx
+++ app/(root)/post/[id]/page.jsx
@@ -1,66 +1,21 @@
 'use client'
-import PostCard from "@/components/cards/PostCard";
-import { useEffect } from "react"
-import { post } from "@/constants/index.js"
-import TopbarPosts from "@/components/shared/TopbarPosts";
-import Link from "next/link";
-import Image from "next/image";
+
+import { useEffect, useState } from "react";
 
 export default function page({ params: { id } }) {
-    /*const post = {
-        id: '1',
-        title: 'postTitle',
-        likesCount: 10,
-        content: '<div><ul><li>Item<li><li>Item<li><li>Item<li></ul> </div>',
-        author: 'authorName',
-        tags: ('tag1, tag2, tag3'),
-    }
-    */
-    //const Title = post.title;
+    const [post, setPost] = useState(null);
 
-    const array = [post, post, post];
-
-    function ShowPost(msg) {
-        useEffect(() => {
-            var logElem = document.querySelector(".log")
-            logElem.innerHTML = msg;
-        });
-    }
-
-    function createMarkup() {
-        return {__html: '<div>Content</div>'};
-      }
-      
+    useEffect(() => {
+        fetch('http://dummyjson.com/products/1')
+            .then((res) => res.json())
+            .then((post_data) => {
+                setPost(post_data)
+            });
+    }, []);
 
     return (
-
-        <div>
-            <arcticle>
-                <div className="border-2 border-slate-300 rounded-lg shadow-md p-3 mt-3">
-                    <ul className="space-y-2">
-                        <div className="flex items-end space-x-1">
-                            <li>
-                                <Link href={'/account/' + post.authorId}>
-                                    <Image src="/assets/profile.svg" width={30} height={30}></Image>
-                                </Link>
-                            </li>
-                            <li>{post.author}</li>
-                        </div>
-                        <div className="flex space-x-1">
-                            {post.badges.map((badge) => (
-                                <li className="px-1 border rounded-lg border-slate-400">{badge}</li>))}
-                        </div>
-                        <li className="text-center">
-                            <h1 className='text-2xl sm:text-3xl my-4 mx-4'>{post.title}</h1>
-                        </li>
-                        <div className='log'>
-                        </div>
-                        <div dangerouslySetInnerHTML={createMarkup()}></div>
-                    </ul>
-                </div>
-            </arcticle>
-
-            <p>{id}</p>
-        </div>
+        <article>
+            <h1>{post?.title}</h1>
+        </article>
     )
 }
--- constants/index.js
+++ constants/index.js
@@ -2,10 +2,10 @@ export const post = {
   id: '1',
   title: 'postTitle',
   titleImg: '/public/assets/profile.svg',
-  likesCount: 10,
   content: '<div><ul><li>Item<li><li>Item<li><li>Item<li></ul></div>',
   author: 'authorName',
   authorId: '1',
+  author_id: '1',
   tags: ['tag1', 'tag2', 'tag3'],
   badges: ['badge1', 'badge2', 'badge3']
 }
